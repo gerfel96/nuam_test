84bb76a804932ba54958baefd18d49e3c0e34f62
gerfel96 <germanfelipe22@gmail.com>
2025-03-06 00:07:15 -0500

    chore: initialize project with basic structure

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL CHECK (char_length(name) >= 3),
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL CHECK (price > 0),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
